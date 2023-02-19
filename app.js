efa16a002f4128bdf4012811e0ba093546d9ff3e
thiengoten <thiengoten6@gmail.com>
2023-02-20 02:54:59 +0700

    feat: 🎸 Add Swagger For Api Docs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const { connectDatabase } = require('./database/connect')
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
 
 var indexRouter = require('./routes/index')
 const usersRouter = require('./routes/api/user')
@@ -21,11 +23,38 @@ app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/tags', tagRouter)
 
-// /users/
-
 //connect to database
 ;(async () => {
     await connectDatabase()
 })()
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'LogRocket Express API with Swagger',
+            version: '0.1.0',
+            description:
+                'This is a simple CRUD API application made with Express and documented with Swagger',
+            license: {
+                name: 'MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+            },
+            contact: {
+                name: 'LogRocket',
+                url: 'https://logrocket.com',
+            },
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/api/*.js'],
+}
+
+const specs = swaggerJsdoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 module.exports = app
--- routes/api/tag.js
+++ routes/api/tag.js
@@ -9,17 +9,166 @@ const {
     deleteTag,
 } = require('../../controllers/TagController')
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Tag:
+ *       type: object
+ *       required:
+ *         - name
+ *         - description
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The title of your book
+ *         description:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         name: Js
+ *         description: Welcom to js
+ *
+ */
+
+/**
+ * @swagger
+ * /tags:
+ *   get:
+ *     tags:
+ *        - Tags
+ *     summary: Retrieve a list of books
+ *     description: Retrieve a list of Books. Can be used to populate a list of fake books when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: A list of books.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 0
+ *                   name:
+ *                     type: string
+ *                     example: Leanne Graham
+ *                   description:
+ *                     type: string
+ *                     example: Test
+ */
 //* get all tags
 router.get('/', getAllTags)
 
+/**
+ * @swagger
+ * /tags/{id}:
+ *   get:
+ *     tags:
+ *        - Tags
+ *     summary: Retrieve a  book
+ *     description: Retrieve a  Books.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *     responses:
+ *       200:
+ *         description: A list of books.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   example: 0
+ *                 name:
+ *                   type: string
+ *                   example: Js
+ *                 description:
+ *                   type: string
+ *                   example: Test
+ *
+ */
 //* get a tag by id
 router.get('/:id', getTagById)
 
+/**
+ * @swagger
+ * tags:
+ *   name: Tags
+ *   description: The books managing API
+ * /tags:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Tags]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Tag'
+ */
 //* create a tag
 router.post('/', createTag)
 
+/**
+ * @swagger
+ * /tags/{id}:
+ *   put:
+ *     tags:
+ *        - Tags
+ *     summary: Update book
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the tag to retrieve.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 name: string
+ *                 example: js
+ *               price:
+ *                 type: string
+ *                 example: test
+ *     responses:
+ *       201:
+ *         description: Update books.
+ */
 //* update a tag
 router.patch('/:id', updateTag)
+
+/**
+ * @swagger
+ * /tags/{id}:
+ *  delete:
+ *      tags:
+ *         - Tags
+ *      summary: Delete tag
+ *      description: Delete tag
+ *      parameters:
+ *        - in: path
+ *          name: tagId
+ *          schema:
+ *              type: string
+ *          required: true
+ *          description: string id of Book to delete
+ *      responses:
+ *          200:
+ *              description: Book that was deleted
+ */
 
 //* delete a tag
 router.delete('/:id', deleteTag)
