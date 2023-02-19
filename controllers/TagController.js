360d9bc6bdf79fc591923dadb6e2e3f3e2e4e43a
thiengoten <72801462+thiengoten@users.noreply.github.com>
2023-02-20 00:11:26 +0700

    Tag CRUD fuction (#1)
    Phuc Khang review code TAG function lear

---
+++ controllers/TagController.js
@@ -0,0 +1,59 @@
+const TagServices = require('../services/TagServices')
+
+const getAllTags = async (req, res, next) => {
+    try {
+        const tags = await TagServices.getAllTags({})
+        res.status(200).json(tags)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const getTagById = async (req, res, next) => {
+    try {
+        if (!req.params.id) return res.status(400).json({ message: 'No id' })
+        const tag = await TagServices.getTagById(req.params.id)
+        res.status(200).json(tag)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const createTag = async (req, res, next) => {
+    try {
+        if (!req.body) return res.status(400).json({ message: 'No data' })
+        const tag = await TagServices.createTag(req.body)
+        res.status(200).json(tag)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const updateTag = async (req, res, next) => {
+    try {
+        if (!req.params.id) return res.status(400).json({ message: 'No id' })
+        if (!req.body) return res.status(400).json({ message: 'No data' })
+        const tag = await TagServices.updateTag(req.params.id, req.body)
+        res.status(200).json(tag)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+const deleteTag = async (req, res, next) => {
+    try {
+        if (!req.params.id) return res.status(400).json({ message: 'No id' })
+        const tag = await TagServices.deleteTag(req.params.id)
+        res.status(200).json(tag)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+module.exports = {
+    getAllTags,
+    createTag,
+    updateTag,
+    getTagById,
+    deleteTag,
+}
