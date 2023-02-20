30b480a74ad0249bb8ee22b87cbbcf3673cb0f47
thiengoten <thiengoten6@gmail.com>
2023-02-21 02:27:23 +0700

    test: 💍 test population in Post Model fix conflict
    change some code in app.js

--- database/models/Post.js
+++ database/models/Post.js
@@ -1,45 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            uppercase: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        },
-        author: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        tags: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Tag',
-            },
-        ],
-        comments: [
-            {
-                content: String,
-                author: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: 'User',
-                },
-            },
-        ],
-        votes: {
-            type: Number,
-            default: 0,
-        },
-        avatar: {
-            type: String,
-            default: '',
-        },
+  {
+    title: {
+      type: String,
+      required: true,
+      uppercase: true,
     },
-    { timestamps: true }
-)
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    tags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tag',
+      },
+    ],
+    comments: [
+      {
+        content: String,
+        author: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+    votes: {
+      type: Number,
+      default: 0,
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+  },
+  { timestamps: true },
+);
 
-module.exports = mongoose.model('Post', postSchema)
+module.exports = mongoose.model('Post', postSchema);
--- repository/TagRepository.js
+++ repository/TagRepository.js
@@ -1,52 +1,52 @@
-const TagModel = require("../database/models/Tag");
+const TagModel = require('../database/models/Tag');
 
 const findAllTags = async () => {
-    const tags = await TagModel.find({});
-    return tags;
+  const tags = await TagModel.find({});
+  return tags;
 };
 
 const findTagById = async (id) => {
-    try {
-        const tag = await TagModel.findById(id);
-        return tag;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const tag = await TagModel.findById(id);
+    return tag;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const createTag = async (tag) => {
-    try {
-        const tag = await TagModel.create(tag);
-        return tag;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const tag = await TagModel.create(tag);
+    return tag;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const updateTag = async (id, tag) => {
-    try {
-        const updateTag = await TagModel.findByIdAndUpdate(id, tag, {
-            new: true,
-        });
-        return updateTag;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const updateTag = await TagModel.findByIdAndUpdate(id, tag, {
+      new: true,
+    });
+    return updateTag;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const deleteTag = async (id) => {
-    try {
-        const deleteTag = await TagModel.findByIdAndDelete(id);
-        return deleteTag;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const deleteTag = await TagModel.findByIdAndDelete(id);
+    return deleteTag;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-    findAllTags,
-    createTag,
-    updateTag,
-    findTagById,
-    deleteTag,
+  findAllTags,
+  createTag,
+  updateTag,
+  findTagById,
+  deleteTag,
 };
